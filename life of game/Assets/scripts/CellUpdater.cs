395d951f33a66c6be75565b40cccd28ddc896104
superbandit <bor@flamikaan.nl>
2016-01-08 12:04:29 +0100

    part of cell movement
    just half of the whole movement piece

---
+++ life of game/Assets/scripts/CellUpdater.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CellUpdater : MonoBehaviour
+{
+    private Grid grid;
+
+    private float updateTimer = 1; //seconds
+    private bool gameStarted = false;
+
+	void Start ()
+    {
+        grid = GameObject.Find("Grid").GetComponent<Grid>();
+	}
+	
+	void Update ()
+    {
+	    if (gameStarted == true)
+        {
+            if (updateTimer > 0)
+            {
+                updateTimer -= Time.deltaTime;
+            }
+            else
+            {
+                UpdateCells();
+            }
+            
+        }
+	}
+
+    private void UpdateCells()
+    {
+        List<Vector2> currentActiveCells = grid.GetAllActiveCellPositions();
+        for (int i = 0; i < currentActiveCells.Count; i++)
+        {
+            NeighbouringCellAmount(currentActiveCells[i]);
+        }
+    }
+
+    private int NeighbouringCellAmount(Vector2 cellposition)
+    {
+        grid.GetNeighbouringCells((int)cellposition.x, (int)cellposition.y);//todo
+    }
+}
---
+++ life of game/Assets/scripts/Grid.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Grid : MonoBehaviour
+{
+    private bool[,] grid = new bool[20,20];
+    void Start()
+    {
+        for(int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                grid[i, j] = false;
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    /// <summary> this returns the neighbouring cells of target cell </summary>
+    public bool[] GetNeighbouringCells(int x, int y)
+    {
+        bool[] neighbouring = new bool[4];
+
+        neighbouring[0] = grid[x, y - 1];
+        neighbouring[1] = grid[x + 1, y - 1];
+        neighbouring[2] = grid[x + 1, y];
+        neighbouring[3] = grid[x + 1, y + 1];
+        neighbouring[4] = grid[x, y + 1];
+        neighbouring[5] = grid[x - 1, y + 1];
+        neighbouring[6] = grid[x - 1, y];
+        neighbouring[7] = grid[x - 1, y - 1];
+
+        return neighbouring;
+    }
+
+    public List<Vector2> GetAllActiveCellPositions()
+    {
+        List<Vector2> activeCells = new List<Vector2>();
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                if(grid[i,j] == true)
+                {
+                    activeCells.Add(new Vector2(i, j));
+                }
+            }
+        }
+        return activeCells;
+    }
+
+}
